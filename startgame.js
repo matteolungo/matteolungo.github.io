fc5af46a413015ce548db01e6899708df707ce1b
Matteo Lungo <matteo.lungo.13@gmail.com>
2022-06-02 01:15:21 +0200

    Added graphical logic

--- startgame.js
+++ startgame.js
@@ -1,11 +1,41 @@
-import * as codes from './codes.json';
-
-window.onload = function main() { 
+window.onload = function main() {
 
     var points = 0;
-    const values = Object.values(jsonData);
-    const randomValue = values[parseInt(Math.random() * values.length)];
+    var guessed = [];
+    
+    fetch("./codes.json")
+        .then(response => response.json())
+        .then(json => {
 
+            function newFlag() {
+
+                const values = Object.values(json);
+                const keys = Object.keys(json);
+                var positions = [1,2,3,4];
+                
+                var randomKey = keys[parseInt(Math.random() * keys.length)];
+                while (guessed.includes(randomKey)) {
+                    randomKey = keys[parseInt(Math.random() * keys.length)];
+                }
+                document.getElementById("flag").src = "flags/" + randomKey + ".png";
+
+                var correctFlag = json[randomKey];
+                var randomPosition = parseInt(Math.random() * positions.length) + 1;
+                positions = positions.filter(p => p !== randomPosition);
+                document.getElementById("button"+randomPosition).innerHTML = correctFlag;
+                
+                for (var i=0; i<3; i++) {
+                    var randomValue = values[parseInt(Math.random() * values.length)];
+                    var randomPosition = positions[parseInt(Math.random() * positions.length)];
+                    positions = positions.filter(p => p !== randomPosition);
+                    document.getElementById("button"+randomPosition).innerHTML = randomValue;
+                }
+                
+            }
+
+            newFlag();
+
+        });
     // $("#failModal").modal();
 
 }
