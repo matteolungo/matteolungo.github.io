ca696446e6fc3ca091b666327be63b04e6def5a5
Matteo Lungo <matteo.lungo.13@gmail.com>
2022-06-01 23:52:51 +0200

    Initial commit

---
+++ startgame.js
@@ -0,0 +1,11 @@
+import * as codes from './codes.json';
+
+window.onload = function main() { 
+
+    var points = 0;
+    const values = Object.values(jsonData);
+    const randomValue = values[parseInt(Math.random() * values.length)];
+
+    // $("#failModal").modal();
+
+}
