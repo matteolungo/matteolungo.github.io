2a5b254e05af344e97c805d396eb660cc1df5c2f
Matteo <matteo.lungo.13@gmail.com>
2023-05-20 21:06:49 +0200

    Sounds on actions, start modal and refactoring

--- startgame.js
+++ startgame.js
@@ -248,74 +248,84 @@ const json = {
     "zm": "Zambia",
     "zw": "Zimbabwe"
 }
-var score = 0;
-var guessed = [];
-var positions = []
-var choices = [];
-var seconds = 5;
+
+let score = 0;
+let guessed = [];
+let positions = []
+let choices = [];
+let seconds = 5;
 const values = Object.values(json);
 const keys = Object.keys(json);
-var modal = new bootstrap.Modal(document.getElementById("modal"));
-var modalText = document.getElementById("modalText");
-var scoreResult = document.getElementById("scoreResult");
-var randomKey, correctFlag, correctButton, lastPress, timer, interval, points;
+let modalStart = new bootstrap.Modal(document.getElementById('modalStart'));
+let modalEnd = new bootstrap.Modal(document.getElementById('modalEnd'));
+let modalText = document.getElementById('modalText');
+let randomKey, correctFlag, correctButton, lastPress, timer, interval, points;
+
+function start() {
+    modalStart.hide();
+    let buttons = document.getElementsByClassName('button');
+    for (const b of buttons) {
+        b.style.visibility = 'visible';
+    }
+    let flag = document.getElementById('flag');
+    flag.style.visibility = 'visible';
+    newFlag();
+}
 
 function newFlag() {
-
     positions = [1, 2, 3, 4];
     choices = [];
     seconds = 5;
 
-    document.getElementById("button1").disabled = false;
-    document.getElementById("button2").disabled = false;
-    document.getElementById("button3").disabled = false;
-    document.getElementById("button4").disabled = false;
-    document.getElementById("timer").innerHTML = "Tempo: " + seconds;
-    document.getElementById("points").innerHTML = "";
+    document.getElementById('button1').disabled = false;
+    document.getElementById('button2').disabled = false;
+    document.getElementById('button3').disabled = false;
+    document.getElementById('button4').disabled = false;
+    document.getElementById('timer').innerHTML = 'Tempo: ' + seconds;
+    document.getElementById('points').innerHTML = '';
     if (lastPress) {
-        lastPress.className = "btn border btn-lg button btn-light";
-        correctButton.className = "btn border btn-lg button btn-light";
+        lastPress.className = 'btn border btn-lg button btn-light';
+        correctButton.className = 'btn border btn-lg button btn-light';
     }
     do {
         randomKey = keys[parseInt(Math.random() * keys.length)];
     } while (guessed.includes(json[randomKey]))
-    document.getElementById("flag").src = "flags/" + randomKey + ".png";
+    document.getElementById('flag').src = 'flags/' + randomKey + '.png';
     correctFlag = json[randomKey];
     choices.push(correctFlag);
-    var randomPosition = parseInt(Math.random() * positions.length) + 1;
+    let randomPosition = parseInt(Math.random() * positions.length) + 1;
     positions = positions.filter(p => p !== randomPosition);
-    correctButton = document.getElementById("button" + randomPosition);
+    correctButton = document.getElementById('button' + randomPosition);
     correctButton.innerHTML = correctFlag;
 
     while (choices.length < 4) {
-        var randomValue = values[parseInt(Math.random() * values.length)];
+        let randomValue = values[parseInt(Math.random() * values.length)];
         if (!choices.includes(randomValue)) {
             choices.push(randomValue);
-            var randomPosition = positions[parseInt(Math.random() * positions.length)];
+            let randomPosition = positions[parseInt(Math.random() * positions.length)];
             positions = positions.filter(p => p !== randomPosition);
-            document.getElementById("button" + randomPosition).innerHTML = randomValue;
+            document.getElementById('button' + randomPosition).innerHTML = randomValue;
         }
     }
 
     interval = setInterval(() => {
         seconds--;
-        document.getElementById("timer").innerHTML = "Tempo: " + seconds;
+        document.getElementById('timer').innerHTML = 'Tempo: ' + seconds;
     }, 1000);
 
     timer = setTimeout(() => {
-        scoreResult.innerHTML = score;
         lose();
         clearInterval(interval);
     }, 5000);
 
 }
 
-function checkResult() {
-    document.getElementById("button1").disabled = true;
-    document.getElementById("button2").disabled = true;
-    document.getElementById("button3").disabled = true;
-    document.getElementById("button4").disabled = true;
-    var answer = event.target.innerHTML;
+function checkResult(event) {
+    document.getElementById('button1').disabled = true;
+    document.getElementById('button2').disabled = true;
+    document.getElementById('button3').disabled = true;
+    document.getElementById('button4').disabled = true;
+    let answer = event.target.innerHTML;
     if (answer === correctFlag) {
         if (seconds > 4) {
             points = 3;
@@ -326,21 +336,21 @@ function checkResult() {
         }
         guessed.push(correctFlag)
         score += points;
-        document.getElementById("score").innerHTML = "Punti: " + score;
-        document.getElementById("points").innerHTML = "+ " + points;
-        event.target.className = "btn border btn-lg button btn-success";
+        document.getElementById('score').innerHTML = 'Punti: ' + score;
+        document.getElementById('points').innerHTML = '+ ' + points;
+        event.target.className = 'btn border btn-lg button btn-success';
         if (guessed.length === 248) {
-            scoreResult.innerHTML = score;
             win();
         } else {
-            setTimeout(() => { newFlag() }, 1300);
+            let audio = new Audio('./res/correct.mp3');
+            audio.play();
+            setTimeout(() => { newFlag() }, 1000);
         }
 
     } else {
-        event.target.className = "btn border btn-lg button btn-danger"
-        correctButton.className = "btn border btn-lg button btn-success";
+        event.target.className = 'btn border btn-lg button btn-danger'
+        correctButton.className = 'btn border btn-lg button btn-success';
         lastPress = event.target;
-        scoreResult.innerHTML = score;
         lose();
     }
     lastPress = event.target;
@@ -350,34 +360,33 @@ function checkResult() {
 
 function restart() {
     score = 0;
-    document.getElementById("score").innerHTML = "Punti: " + score;
+    document.getElementById('score').innerHTML = 'Punti: ' + score;
     guessed = [];
-    modal.hide();
+    modalEnd.hide();
     newFlag();
 }
 
 function lose() {
-    modalText.innerHTML = "Hai perso!";
-    modal.toggle();
+    let audio = new Audio('./res/lose.mp3');
+    audio.play();
+    modalText.innerHTML = `Hai perso!<br/>Hai totalizzato ${score} punti.`;
+    modalEnd.toggle();
 }
 
 function win() {
-    modalText.innerHTML = "Hai vinto!";
-    modal.toggle();
-}
-
-const sleepSync = (ms) => {
-    const end = new Date().getTime() + ms;
-    while (new Date().getTime() < end) { }
+    let audio = new Audio('./res/win.mp3');
+    audio.play();
+    modalText.innerHTML = `Hai vinto!<br/>Hai totalizzato ${score} punti.`;
+    modalEnd.toggle();
 }
 
 window.onload = function main() {
-
-    document.getElementById("button1").onclick = checkResult;
-    document.getElementById("button2").onclick = checkResult;
-    document.getElementById("button3").onclick = checkResult;
-    document.getElementById("button4").onclick = checkResult;
-    document.getElementById("restartButton").onclick = restart;
-    newFlag();
+    document.getElementById('button1').addEventListener('click', checkResult);
+    document.getElementById('button2').addEventListener('click', checkResult);
+    document.getElementById('button3').addEventListener('click', checkResult);
+    document.getElementById('button4').addEventListener('click', checkResult);
+    document.getElementById('restartButton').addEventListener('click', restart);
+    document.getElementById('startButton').addEventListener('click', start);
+    modalStart.toggle();
 }
 
