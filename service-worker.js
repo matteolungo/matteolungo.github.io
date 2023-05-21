af0907831ee4814528fbeefcfd73753dd1a5cafb
Matteo <matteo.lungo.13@gmail.com>
2023-05-21 22:08:11 +0200

    Bugfix service worker

--- service-worker.js
+++ service-worker.js
@@ -48,7 +48,7 @@ self.addEventListener('fetch', event => {
         }
 
         return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
+          return fetch(event.request, {cache: "no-store"}).then(response => {
             // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
               return response;
